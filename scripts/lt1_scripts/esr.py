be9e4514143d3a468196409b11cca6dada477a04
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2012-12-27 14:45:58 +0100

    did some measurements on lt1

--- scripts/lt1_scripts/esr.py
+++ scripts/lt1_scripts/esr.py
@@ -1,40 +1,29 @@
 import qt
-import data
-from measurement.measurement import Measurement
-import measurement.PQ_measurement_generator_v2 as pqm
 import msvcrt
 # from measurement.AWG_HW_sequencer_v2 import Sequence
 
-name='ESR_SIL2_LT1'
-start_f = 2.820 #   2.853 #2.85 #  #in GHz
-stop_f  = 2.838 #   2.864 #2.905 #   #in GHz
-steps   = 101
-mw_power_lt1 = -10  #in dBm
-mw_power_lt2 = -12   #in dBm
+name='ESR_SIL3_LT1'
+start_f = 2.83 #   2.853 #2.85 #  #in GHz
+stop_f  = 2.91 #   2.864 #2.905 #   #in GHz
+steps   = 161
+mw_power = -10  #in dBm
 int_time = 30       #in ms
-reps = 10
-
-lt1 = True
+reps = 5
 
 #generate list of frequencies
 f_list = linspace(start_f*1e9, stop_f*1e9, steps)
 
-if lt1:
-    ins_smb = qt.instruments['SMB100']
-    ins_adwin = qt.instruments['adwin']
-    ins_counters = qt.instruments['counters']
-    counter = 2
-    MW_power = mw_power_lt1
-else:
-    pass
-    #ins_smb = qt.instruments['SMB100']
-    #ins_adwin = qt.instruments['adwin']
-    #ins_counters = qt.instruments['counters']
-    #counter = 1
-    #MW_power = mw_power_lt2
+ins_smb = qt.instruments['SMB100']
+ins_adwin = qt.instruments['adwin']
+ins_counters = qt.instruments['counters']
+counter = 2
+MW_power = mw_power
+
+ins_counters.set_is_running(0)
 
 # create data object
 qt.mstart()
+
 
 ins_smb.set_iq('off')
 ins_smb.set_pulm('off')
@@ -45,6 +34,9 @@ qt.msleep(0.2)
 #ins_counters.set_is_running(0)
 total_cnts = zeros(steps)
 for cur_rep in range(reps):
+    
+    print 'sweep %d/%d ...' % (cur_rep+1, reps)
+    
     for i,cur_f in enumerate(f_list):
         
         ins_smb.set_frequency(cur_f)
@@ -55,22 +47,20 @@ for cur_rep in range(reps):
 
     p_c = qt.Plot2D(f_list, total_cnts, 'bO-', name=name, clear=True)
     if (msvcrt.kbhit() and (msvcrt.getch() == 'q')): break
-    print cur_rep
+    
+
 ins_smb.set_status('off')
 
 d = qt.Data(name=name)
 d.add_coordinate('frequency [GHz]')
 d.add_value('counts')
 d.create_file()
-
 filename=d.get_filepath()[:-4]
 
 d.add_data_point(f_list, total_cnts)
-pqm.savez(filename,freq=f_list,counts=total_cnts)
 d.close_file()
 p_c = qt.Plot2D(d, 'bO-', coorddim=0, name=name, valdim=1, clear=True)
 p_c.save_png(filename+'.png')
-
 
 qt.mend()
 
