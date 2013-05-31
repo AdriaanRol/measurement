d96fa6e3d35813e94a2e02fd65a72f494ed6b3f2
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2013-05-31 10:52:19 +0200

    separated the static lt2 settings fpor the m2 classes from the reload all script into a dedicated file

--- scripts/lt2_scripts/m2/reload_all.py
+++ scripts/lt2_scripts/m2/reload_all.py
@@ -31,22 +31,4 @@ reload(mbi_espin)
 execfile(r'd:\measuring\measurement\scripts\lt2_scripts\m2\msmt_params.py')
 
 # set all the static variables for lt2
-ssro.AdwinSSRO.adwin_processes_key = 'adwin_lt2_processes'
-ssro.AdwinSSRO.E_aom = qt.instruments['MatisseAOM']
-ssro.AdwinSSRO.A_aom = qt.instruments['NewfocusAOM']
-ssro.AdwinSSRO.green_aom = qt.instruments['GreenAOM']
-ssro.AdwinSSRO.yellow_aom = qt.instruments['YellowAOM']
-ssro.AdwinSSRO.adwin = qt.instruments['adwin']
-ssro.AdwinSSRO.repump_aom = qt.instruments['GreenAOM']
-
-sequence.SequenceSSRO.awg = qt.instruments['AWG']
-sequence.SequenceSSRO.mwsrc = qt.instruments['SMB100']
-sequence.SequenceSSRO.chan_mwI = 'MW_Imod'
-sequence.SequenceSSRO.chan_mwQ = 'MW_Qmod'
-sequence.SequenceSSRO.chan_mw_pm = 'MW_pulsemod'
-sequence.SequenceSSRO.awgcfg_module = awgcfg
-sequence.SequenceSSRO.awgcfg_args = ['spin']
-
-mbi.MBIMeasurement.chan_RF  = 'RF' 
-mbi.MBIMeasurement.chan_adwin_sync = 'ADwin_trigger'
-mbi.MBIMeasurement.physical_adwin = qt.instruments['physical_adwin']
+execfile(r'd:\measuring\measurement\scripts\lt2_scripts\m2\lt2_statics.py')
